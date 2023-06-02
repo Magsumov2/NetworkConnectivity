e23e1e004a308acd4a17c0f9841a2e883ba4a31d
Musa <musamagsumov2@gmail.com>
2023-06-02 11:10:04 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NetworkConnectivity
+
+A description of this package.
---
+++ Sources/NetworkConnectivity/NetworkConnectivity.swift
@@ -0,0 +1,6 @@
+public struct NetworkConnectivity {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
